0c67ad459f295eee72e159e87fb94bd10ff7675a
Maxim Chiper <maxchiper2@gmail.com>
2023-12-04 15:36:45 +0200

    mai am animatia la intoarcere si cvcul

--- main.js
+++ main.js
@@ -20,8 +20,56 @@ setTimeout(()=>{
 
 
 
-document.querySelector('#nrs-on-card').oninput= ()=>{
-    const nrsOnCard = document.querySelector('#nrs-on-card')
-    const output =  document.querySelector('#card-nr')
-    nrsOnCard.value = output.value
-}
+
+const cardNumber = document.querySelector('#nrs-on-card')
+let txtCardNumber = document.querySelector("#card-nr");
+
+txtCardNumber.addEventListener("input", (e)=> {
+    let cardNumber = txtCardNumber.value;
+
+    // Do not allow users to write invalid characters
+    let formattedCardNumber = cardNumber.replace(/[^\d]/g, "");
+    formattedCardNumber = formattedCardNumber.substring(0, 16);
+
+    // Split the card number is groups of 4
+    let cardNumberSections = formattedCardNumber.match(/\d{1,4}/g);
+    if (cardNumberSections !== null) {
+        formattedCardNumber = cardNumberSections.join(' ');
+    }
+
+    console.log("'"+ cardNumber + "' to '" + formattedCardNumber + "'");
+
+    // If the formmattedCardNumber is different to what is shown, change the value
+    if (cardNumber !== formattedCardNumber) {
+        txtCardNumber.value = formattedCardNumber;
+    }
+})
+txtCardNumber.addEventListener('input', (ev)=> {
+    const inputValue = ev.target.value
+    cardNumber.value = inputValue
+})
+
+const cardName = document.querySelector('#card-name')
+const cardNameOnCard = document.querySelector("#name-on-card")
+
+cardName.addEventListener('input', (ev)=>{
+    const inputValue = ev.target.value
+    cardNameOnCard.value = inputValue
+})
+
+
+const yearMonthOnCard = document.querySelector('#year-mounth-on-card')
+const month  = document.querySelector('#month')
+
+
+const year = document.querySelector('#year')
+
+ year.addEventListener('input', (ev)=>{
+    const inputValue2 = ev.target.value
+     inputOnyear +=inputValue2
+})
+let inputOnyear = ''
+month.addEventListener('input', (ev)=>{
+    const inputValue = ev.target.value
+    yearMonthOnCard.value = inputValue+'/'+inputOnyear
+})
